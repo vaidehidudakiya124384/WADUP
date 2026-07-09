2a2452e19f90944774082ee2d1e808c6eab3a2e1
vaidehidudakiya124384 <vaidehi.dudakiya124384@marwadiuniversity.ac.in>
2026-07-09 08:44:28 +0530

    Add files via upload

---
+++ Get&Set_Type.php
@@ -0,0 +1,11 @@
+<?php
+
+    $number = 10;
+    settype($number,"string");
+    echo gettype( $number ) ."<br>";
+
+    $name = "vaidehi";
+    gettype($name);
+    echo gettype( $name ) ."<br>";
+
+?>
